2bd32c9a3e3f8eae4bfc99783513c6f55093f6e4
nguyentaan <vantaan2002@gmail.com>
2024-04-16 19:46:42 +0700

    Lab03

---
+++ Lab03/bai1/app/src/main/java/com/example/bai1/GraphicsView.java
@@ -0,0 +1,25 @@
+package com.example.bai1;
+
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.view.MotionEvent;
+import android.graphics.Color;
+import android.view.View;
+import android.graphics.Canvas;
+import android.content.Context;
+
+public class GraphicsView extends View {
+    public GraphicsView (Context context){
+        super(context);
+    }
+    @Override
+    protected void onDraw(Canvas canvas){
+            Rect r = new Rect(40, 40, 400, 200);
+            Paint paint = new Paint();
+            paint.setStyle(Paint.Style.FILL);
+            paint.setColor(Color.RED);
+            canvas.drawRect(r, paint);
+        invalidate();
+    }
+
+}
